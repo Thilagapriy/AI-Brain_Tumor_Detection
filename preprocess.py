8d2f1e9ade0db211475a4c23ed5f456fb5366be0
SK <aishwaryat701@gmail.com>
2025-12-27 02:48:09 +0530

    Add files via upload

---
+++ preprocess.py
@@ -0,0 +1,20 @@
+from torchvision import transforms
+
+# For validation/testing
+preprocess = transforms.Compose([
+    transforms.Resize((224, 224)),
+    transforms.ToTensor(),
+    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+])
+
+# For training (with augmentation)
+augment_train = transforms.Compose([
+    transforms.RandomHorizontalFlip(p=0.5),
+    transforms.RandomRotation(degrees=15),
+    transforms.ColorJitter(brightness=0.2, contrast=0.2),
+    transforms.Resize((224, 224)),
+    transforms.ToTensor(),
+    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+])
+
+print("Preprocessing and augmentation pipelines ready.")
